71541397f89cf70de36135cfb4622a5a771c0a85
João Vinícius Rosá <rosajv.blu@gmail.com>
2023-09-05 09:02:38 -0300

    Good

--- JavaOOP/JavaOOP/src/Atividade0509/Maindots.java
+++ JavaOOP/JavaOOP/src/Atividade0509/Maindots.java
@@ -1,5 +1,15 @@
 package Atividade0509;
 
 public class Maindots {
+    public static void main(String[] args) {
+        var apolica = new Apolice();
 
+        apolica.setName("Amongus");
+        apolica.setAge(41);
+        apolica.setAwardValue(69420);
+
+        apolica.calculateAwardValue();
+        System.out.println(apolica.toString());
+        apolica.tradeOfferDiscount("Hiroshima");
+    }
 }
